e3e6e2581c2e4e22630aba3cee14691d9620012d
Nguyen Vi <hoangvi.due@gmail.com>
2022-05-08 19:49:05 +0700

    Complete Level_10_Data_Table_I

--- testcases/com/jquery/datatable/Level_10_DataTable_DataGrid.java
+++ testcases/com/jquery/datatable/Level_10_DataTable_DataGrid.java
@@ -17,8 +17,8 @@ import pageObject.jQuery.PageGeneratorManager;
 public class Level_10_DataTable_DataGrid extends BaseTest {
 	private WebDriver driver;
 	HomePageObject homePage;
-	List <String> actualAllCountryValues;
-	List <String> expectedAllCountryValues;
+	List<String> actualAllCountryValues;
+	List<String> expectedAllCountryValues;
 
 	@Parameters("browser")
 	@BeforeClass
@@ -28,43 +28,41 @@ public class Level_10_DataTable_DataGrid extends BaseTest {
 
 	}
 
-//	@Test
+	@Test
 	public void Table_01_Paging() {
 		homePage.openPagingByPageNumber("10");
 		homePage.sleepInSecond(2);
-		Assert.assertTrue (homePage.isPageNumberActived("10"));
+		Assert.assertTrue(homePage.isPageNumberActived("10"));
 		homePage.sleepInSecond(2);
-		
+
 		homePage.openPagingByPageNumber("15");
 		homePage.sleepInSecond(2);
-		Assert.assertTrue (homePage.isPageNumberActived("15"));
-		
+		Assert.assertTrue(homePage.isPageNumberActived("15"));
+
 		homePage.openPagingByPageNumber("7");
 		homePage.sleepInSecond(2);
-		Assert.assertTrue (homePage.isPageNumberActived("7"));
-		
+		Assert.assertTrue(homePage.isPageNumberActived("7"));
+
 		homePage.openPagingByPageNumber("20");
 		homePage.sleepInSecond(2);
-		Assert.assertTrue (homePage.isPageNumberActived("20"));
-		
-		
+		Assert.assertTrue(homePage.isPageNumberActived("20"));
+
 	}
 
-//	@Test
+	@Test
 	public void Table_02_Enter_To_Header() {
 		homePage.refreshCurrentPage(driver);
-		
+
 		homePage.enterToHeaderTextboxByLabel("Country", "Argentina");
 		homePage.sleepInSecond(2);
-		
-		
+
 		homePage.enterToHeaderTextboxByLabel("Females", "15999");
 		homePage.sleepInSecond(2);
 		homePage.enterToHeaderTextboxByLabel("Males", "407124");
 		homePage.sleepInSecond(2);
 		homePage.enterToHeaderTextboxByLabel("Total", "12599691");
 		homePage.sleepInSecond(2);
-		
+
 		homePage.enterToHeaderTextboxByLabel("Country", "Angola");
 		homePage.sleepInSecond(2);
 		homePage.enterToHeaderTextboxByLabel("Females", "764956");
@@ -73,18 +71,16 @@ public class Level_10_DataTable_DataGrid extends BaseTest {
 		homePage.sleepInSecond(2);
 		homePage.enterToHeaderTextboxByLabel("Total", "553353");
 		homePage.sleepInSecond(2);
-	
+
 	}
-	
+
 	@Test
 	public void Table_03_() {
 		actualAllCountryValues = homePage.getValueEachRowAtAllPage();
-		
-//		Assert.assertEquals(actualAllCountryValues, expectedAllCountryValues);
-		
-		
-	}
 
+//		Assert.assertEquals(actualAllCountryValues, expectedAllCountryValues);
+
+	}
 
 	@AfterClass
 	public void afterClass() {
